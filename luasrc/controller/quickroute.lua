4ba5214b99950a09cac99cf6ad6fd12963083d0f
flakeforever <113103193+flakeforever@users.noreply.github.com>
2022-09-23 23:47:38 +0800

    Add files via upload

--- luasrc/controller/quickroute.lua
+++ luasrc/controller/quickroute.lua
@@ -5,10 +5,10 @@ function index()
 		return
 	end
 
-	local page = entry({"admin", "network", "quickroute"},alias("admin", "network", "quickroute", "overview"), _("Quick Route"), 90)
+	local page = entry({"admin", "network", "quickroute"},alias("admin", "network", "quickroute", "general"), _("Quick Route"), 90)
 	
-	entry({"admin", "network", "quickroute", "overview"},cbi("quickroute/overview"),_("Overview"), 10).leaf = true
-	entry({"admin", "network", "quickroute", "interface"},cbi("quickroute/interface"),_("Interface"), 20).leaf = true
+	entry({"admin", "network", "quickroute", "general"},cbi("quickroute/general"),_("Settings"), 10).leaf = true
+	entry({"admin", "network", "quickroute", "interfaces"},cbi("quickroute/interfaces"),_("Interfaces"), 20).leaf = true
 	entry({"admin", "network", "quickroute", "mangle"},cbi("quickroute/mangle"),_("Mangle"), 30).leaf = true
 end
 
